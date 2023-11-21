1c78d5d2c86587f9d787c858c423f6ed6c76d684
drseu55 <alpics1337@gmail.com>
2023-11-21 13:46:15 +0200

    feat: Catch db connection error

--- src/db/connect.js
+++ src/db/connect.js
@@ -7,12 +7,18 @@ const URL = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}`;
 const client = new MongoClient(URL, { family: 4 });
 
 const createConnection = async () => {
-  await client.connect();
-  console.log("Connected successfully to database");
+  try {
+    await client.connect();
+    console.log("Connected successfully to database");
 
-  const db = client.db(process.env.DB_NAME);
+    const db = client.db(process.env.DB_NAME);
 
-  return db;
+    return db;
+  } catch (error) {
+    // Log to console, because there is no logger
+    console.error("Error when connecting to database, exiting...");
+    process.exit(1);
+  }
 };
 
 const db = await createConnection();
